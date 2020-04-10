d54ae9a88d44b05071d78d20dbb6ce3d07db86a6
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-04-09 18:03:45 -0600

    Intial

---
+++ March-AN-2/Assets/Scripts/Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float m_speed;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
+    }
+}
