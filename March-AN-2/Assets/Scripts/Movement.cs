60ef8d2de6cdde6e5aef61a30cc3910c3e86da29
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-04-14 18:49:34 -0600

    Session 5

--- March-AN-2/Assets/Scripts/Movement.cs
+++ March-AN-2/Assets/Scripts/Movement.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float m_speed;
-   
-    // Update is called once per frame
+    public float m_moveSpeed = 3.5f;
+    public float m_turnSpeed = 15;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
-        transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
+        #region Translation
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * m_moveSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * m_moveSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * m_moveSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * m_moveSpeed * Time.deltaTime);
+        }
+        #endregion
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * m_turnSpeed, Space.World);
+        transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * m_turnSpeed);
     }
 }
