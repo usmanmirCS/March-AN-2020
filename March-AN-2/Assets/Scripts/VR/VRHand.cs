cd81ce6e62784b18e16c7bad9b8b56774b508f44
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-04-30 18:09:39 -0600

    Session 9

---
+++ March-AN-2/Assets/Scripts/VR/VRHand.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHand : MonoBehaviour
+{
+    public Animator m_anim;
+    public string m_gripName;
+
+    private bool m_gripHeld;
+    private GameObject m_touchingObject;
+    private GameObject m_heldObject;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.GetComponent<Rigidbody>())
+        {
+            m_touchingObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_touchingObject = null;
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis(m_gripName) > 0.5f && !m_gripHeld)
+        {
+            m_gripHeld = true;
+            m_anim.SetBool("isGrabbing", true);
+            if(m_touchingObject)
+            {
+                Grab();
+            }
+        }
+        else if(Input.GetAxis(m_gripName) < 0.5f && m_gripHeld)
+        {
+            m_gripHeld = false;
+            m_anim.SetBool("isGrabbing", false);
+            if(m_heldObject)
+            {
+                Release();
+            }
+        }
+    }
+
+    void Grab()
+    {
+        m_heldObject = m_touchingObject;
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        m_heldObject.transform.SetParent(transform);
+    }
+
+    void Release()
+    {
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        m_heldObject.transform.SetParent(null);
+        m_heldObject = null;
+    }
+}
