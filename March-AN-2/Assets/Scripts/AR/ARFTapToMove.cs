440ed12fa26f5898358f91a023af7d1840494fea
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-06-04 18:03:06 -0600

    Session 18

---
+++ March-AN-2/Assets/Scripts/AR/ARFTapToMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARFTapToMove : MonoBehaviour
+{
+    public GameObject m_prefabObject;
+
+    private Transform m_placedTransform;
+
+    static List<ARRaycastHit> s_hits = new List<ARRaycastHit>();
+
+    public ARRaycastManager m_arRaycastManager;
+
+    private void Awake()
+    {
+        m_arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (m_arRaycastManager.Raycast(touch.position, s_hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = s_hits[0].pose;
+                
+                if(!m_placedTransform)
+                {
+                    m_placedTransform = Instantiate(m_prefabObject, hitPose.position, hitPose.rotation).transform;
+                }
+                else
+                {
+                    m_placedTransform.position = hitPose.position;
+                }
+            }
+        }
+    }
+}
